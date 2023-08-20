84215bf437ec4ef38114f338504202eb100d3b7b
darshan <darshpat01@gmail.com>
2023-08-20 22:37:16 +0530

    changed stake and added reward modal

--- src/Pages/Rewards.js
+++ src/Pages/Rewards.js
@@ -1,16 +1,95 @@
 import React from "react";
 import RedeemItem from "./RedeemItem";
 
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useDisclosure,
+} from "@chakra-ui/react";
+
 const addRewardButtonStyle =
   "ml-auto font-semibold bg-gray-300 p-2 rounded-lg hover:scale-110 transition duration-500 ease-in-out transform hover:cursor-pointer";
 
 function Rewards() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
+  const [name, setName] = React.useState("");
+  const [cost, setCost] = React.useState("");
+  const [img, setImg] = React.useState("");
+
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleCostChange = (event) => setCost(event.target.value);
+  const handleImgChange = (event) => setImg(event.target.value);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(name, cost, img);
+
+    // enter code here
+    setImg("");
+    setName("");
+    setCost("");
+    onClose();
+  };
+
   return (
     <>
       <div className="bg-white shadow-lg p-4 w-[55em]">
         <div className="flex">
           <div className="font-bold text-lg">Uploaded Rewards:</div>
-          <div className={addRewardButtonStyle}>Add Reward</div>
+          <div className={addRewardButtonStyle} onClick={onOpen}>
+            Add Reward
+          </div>
+          <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Add Rewards</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>Img</FormLabel>
+                  <Input id="image" value={img} onChange={handleImgChange} />
+                </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Name</FormLabel>
+                  <Input id="name" value={name} onChange={handleNameChange} />
+                </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Cost</FormLabel>
+                  <Input id="cost" value={cost} onChange={handleCostChange} />
+                </FormControl>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button
+                  colorScheme="blue"
+                  mr={3}
+                  onClick={(e) => {
+                    submitHandler(e);
+                  }}
+                >
+                  Save
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </div>
         <div className="pt-2 flex flex-wrap justify-evenly ">
           {/* image of product */}
